63d34903d22527be49430395cd35126e53b71738
Seth Clapp <seth.clapp22@gmail.com>
2023-09-05 17:40:07 -0500

    My first commit

---
+++ helloWorld.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 <?php echo "style='color:red';"?>>WDV341 Intro PHP</h1>
+    <h2>UNIT-2 PHP Basics <?php echo "Welcome to PHP"; ?></h2>
+    <?php
+    
+    //This is a PHP code block
+    echo "Hello World!"; //document.write()
+
+    echo "<p>This is a paragraph.</p>";
+    echo "<ol> <li>Item 1</li></ol>";
+    ?>
+</body>
+</html>
